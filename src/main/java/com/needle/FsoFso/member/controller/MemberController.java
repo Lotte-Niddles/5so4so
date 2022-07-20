615258d77e0231a65820f046f6b9954cbe20200a
Solji Choi <bssj9307@gmail.com>
2022-07-20 18:07:36 +0900

    Feat: admin users 추가 및 로그인시 경로 설정

--- src/main/java/com/needle/FsoFso/member/controller/MemberController.java
+++ src/main/java/com/needle/FsoFso/member/controller/MemberController.java
@@ -3,9 +3,8 @@ package com.needle.FsoFso.member.controller;
 import com.needle.FsoFso.member.kakao.dto.KakaoOauthInfo;
 import com.needle.FsoFso.member.service.Member;
 import com.needle.FsoFso.member.service.MemberService;
-import java.io.IOException;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +16,13 @@ public class MemberController {
 
     private final MemberService memberService;
     private final KakaoOauthInfo kakaoOauthInfo;
+    private final AdminMembers adminMembers;
 
-    public MemberController(MemberService memberService, KakaoOauthInfo kakaoOauthInfo) {
+    public MemberController(MemberService memberService, KakaoOauthInfo kakaoOauthInfo,
+            AdminMembers adminMembers) {
         this.memberService = memberService;
         this.kakaoOauthInfo = kakaoOauthInfo;
+        this.adminMembers = adminMembers;
     }
 
     @GetMapping("/login.do")
@@ -30,21 +32,24 @@ public class MemberController {
     }
 
     @GetMapping("/oauth.do")
-    public void oauthRedirect(String code, HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    public String oauthRedirect(String code, HttpServletRequest request) {
         final Member member = memberService.login(code);
         request.getSession().setAttribute("member", member);
 
-        response.sendRedirect("/productList.do");
+        final List<String> adminUsers = adminMembers.getAdminUsers();
+        if (adminUsers.contains(member.getProviderId().toString())) {
+            return "redirect:/admin.do";
+        }
+
+        return "redirect:/productList.do";
     }
 
     @GetMapping("/logout.do")
-    public void logout(Long id, HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    public String logout(Long id, HttpServletRequest request) {
         final boolean logout = memberService.logout(id);
         if (logout) {
             request.getSession().removeAttribute("member");
         }
-        response.sendRedirect("/productList.do");
+        return "redirect:/productList.do";
     }
 }
