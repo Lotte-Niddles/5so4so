199163eae8a3b3df6acf138079739c8c524bfe45
Solji Choi <bssj9307@gmail.com>
2022-07-22 14:15:01 +0900

    Feat: 에러 발생시 리다이렉트 페이지 추가

--- src/main/java/com/needle/FsoFso/common/ui/CommonController.java
+++ src/main/java/com/needle/FsoFso/common/ui/CommonController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CommonController {
 
     @GetMapping("/404.do")
+    public String showPageNotFound() {
+        return "404.tiles";
+    }
+
+    @GetMapping("/error.do")
     public String showErrorPage() {
         return "error.tiles";
     }
